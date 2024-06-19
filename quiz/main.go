56b7a23b94f2d9a81b824dcbb3666e704ca4d3f6
Karlil Abolins <karlilashraf@gmail.com>
2024-06-19 16:23:40 +0800

    Fixed timer to ensure it will run out without having to wait for an input

--- quiz/main.go
+++ quiz/main.go
@@ -31,14 +31,17 @@ func main() {
 
 	correct := 0
 	for i, p := range problems {
-		select {
-		case <-timer.C:
-			fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
-			return
-		default:
-			fmt.Printf("Problem #%d; %s = ", i+1, p.q)
+		fmt.Printf("Problem #%d; %s = ", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
 			var answer string
 			fmt.Scanf("%s\n", &answer)
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou scored %d out of %d.\n", correct, len(problems))
+			return
+		case answer := <-answerCh:
 			if answer == p.a {
 				correct++
 			}
